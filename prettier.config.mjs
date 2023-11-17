3b102632a9dcd894edc33481dd257258455b8930
jatobro <jakob.tossebro@gmail.com>
2023-11-17 19:22:14 +0100

    tailwindcss prettier plugin now working

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,16 +1,8 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
-  arrowParens: "always",
-  printWidth: 80,
-  singleQuote: false,
-  jsxSingleQuote: false,
-  semi: true,
-  trailingComma: "all",
-  tabWidth: 2,
   plugins: [
     "prettier-plugin-tailwindcss",
-    "prettier-plugin-css-order",
     "prettier-plugin-organize-imports",
+    "prettier-plugin-css-order",
   ],
 };
 
