29011030fd3f4228ed53d5c10773bdf33e4b91c6
jatobro <jakob.tossebro@gmail.com>
2023-12-23 00:07:40 +0100

    setup clerk

--- src/server/api/routers/programme.ts
+++ src/server/api/routers/programme.ts
@@ -1,7 +1,17 @@
+import z from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const programmeRouter = createTRPCRouter({
-  getAll: publicProcedure.query(async ({ ctx }) => {
+  getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.programme.findMany();
   }),
+  getByName: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.db.programme.findFirstOrThrow({
+        where: {
+          name: input.name,
+        },
+      });
+    }),
 });
