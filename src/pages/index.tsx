2d0752bd799dafc63bcd44a01935c0dacc18123d
jatobro <jakob.tossebro@gmail.com>
2023-12-24 15:08:10 +0100

    added logo to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { SearchBar } from "~/components/SearchBar";
 
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
@@ -10,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-slate-200 dark:bg-slate-800 flex flex-col items-center justify-around flex-1">
+        <Image src="/logo.svg" alt="logo" width={300} height={300} />
         <h1 className="text-5xl dark:text-slate-200">
           Guess today&apos;s study programme!
         </h1>
