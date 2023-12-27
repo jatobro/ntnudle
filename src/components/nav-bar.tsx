61dc5eb807945f63b54e4081283c259ef3330495
jatobro <jakob.tossebro@gmail.com>
2023-12-27 04:04:06 +0100

    sign in button

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,9 +1,14 @@
 import { UserButton, useUser } from "@clerk/nextjs";
 import Link from "next/link";
+
+import { useRouter } from "next/router";
 import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
 
 export const NavBar = () => {
   const { isLoaded, isSignedIn } = useUser();
+
+  const router = useRouter();
 
   return (
     <header className="flex flex-row gap-2 bg-muted text-muted-foreground p-1 h-12">
@@ -19,7 +24,7 @@ export const NavBar = () => {
       </Link>
       <div className="flex-1 flex justify-end items-center">
         {isLoaded ? (
-          isSignedIn ? (
+          isSignedIn || router.asPath !== "/" ? (
             <UserButton
               afterSignOutUrl="/"
               showName={true}
@@ -32,7 +37,7 @@ export const NavBar = () => {
               }}
             />
           ) : (
-            <Link href="/sign-in">Sign In</Link>
+            <Button onClick={() => router.push("/sign-in")}>Sign In</Button>
           )
         ) : null}
       </div>
